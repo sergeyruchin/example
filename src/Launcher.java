9c0dc1178fe98e425ab2c45707d094e42d03a852
Sergey <casper_net@mail.ru>
2023-05-13 12:51:45 +0300

    Launcher update 1 and .gitignore

---
+++ src/Launcher.java
@@ -0,0 +1,15 @@
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        System.out.println(add(a, b));
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
+}
