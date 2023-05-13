8ebeebed656dc2bfdd301916f90b6e4bfe1b0551
Sergey <casper_net@mail.ru>
2023-05-13 12:54:40 +0300

    Launcher mul function

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,6 +7,7 @@ public class Launcher {
 
         System.out.println(add(a, b));
         System.out.println(sub(a, b));
+        System.out.println(mul(a, b));
     }
 
     private static int add(int a, int b) {
@@ -17,4 +18,7 @@ public class Launcher {
         return a - b;
     }
 
+    private static int mul(int a, int b) {
+        return a * b;
+    }
 }
